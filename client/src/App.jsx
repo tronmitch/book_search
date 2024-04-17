3df6f3988414a5da46966a9148d632dc7e538318
tronmitch <theron.mitchell@gmail.com>
2024-04-16 21:51:30 -0600

    apollo server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,26 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink  } from '@apollo/client';
 import Navbar from './components/Navbar';
+import { setContext } from '@apollo/client/link/context'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql', // Make sure this URI is correct
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 const client = new ApolloClient({
-  uri: '/graphql',
-  link: httpLink,
-  cache: new InMemoryCache()
-})
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
